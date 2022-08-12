10cc282497321c3453aa96ae456374fc498aa0eb
180686 <180686@gree.com>
2022-08-12 10:45:50 +0800

    fix():Optimize UserList api

--- imservice/src/main/java/org/hints/im/controller/TestEndPointController.java
+++ imservice/src/main/java/org/hints/im/controller/TestEndPointController.java
@@ -1,9 +1,11 @@
 package org.hints.im.controller;
 
 import org.hints.im.pojo.ReturnVo;
+import org.hints.im.pojo.entity.HistoryDO;
 import org.nutz.dao.Cnd;
 import org.nutz.dao.Dao;
 import org.nutz.dao.entity.Record;
+import org.nutz.dao.util.cri.Static;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,9 +45,12 @@ public class TestEndPointController {
     }
 
     @GetMapping("/history")
-    public ReturnVo history(HashMap map, Principal principal) {
-        List<Record> list = dao.query("sys_user", null);
-        return ReturnVo.success();
+    public ReturnVo history(Integer fromUsername, Principal principal) {
+        String name = principal.getName();
+        List<HistoryDO> query = dao.query(HistoryDO.class, Cnd.where(new Static("((from_id = "+ name +" and to_id = "+ fromUsername +") or (from_id = "+ fromUsername +" and to_id = "+ name +"))")));
+        return ReturnVo.success(query);
     }
+
+
 
 }
