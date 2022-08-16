c4f817d6aa731ca39c9f7680b5244a6ff734d979
180686 <180686@gree.com>
2022-08-16 17:45:18 +0800

    fix():add kafka

---
+++ imservice/src/main/java/org/hints/im/mq/KafkaConsumer.java
@@ -0,0 +1,44 @@
+package org.hints.im.mq;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.hints.im.persist.DataBaseStore;
+import org.hints.im.pojo.MsgBody;
+import org.hints.im.utils.AsyncFactory;
+import org.hints.im.utils.AsyncManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description TODO
+ * @Author 180686
+ * @Date 2022/8/15 14:34
+ */
+@Component
+@Slf4j
+public class KafkaConsumer {
+
+    @Autowired
+    private MongoOperations mongoOperations;
+
+
+
+    @KafkaListener(topics = "test")
+    public void listenZhugeGroup(ConsumerRecord<String, String> record) {
+        String value = record.value();
+
+        MsgBody msgBody = JSONObject.parseObject(value, MsgBody.class);
+
+        log.info("toMongodb ...");
+        mongoOperations.save(msgBody,"message");
+
+        log.info("persistence ...");
+        AsyncManager.me().execute(AsyncFactory.asyncStore(msgBody));
+
+    }
+
+
+}
